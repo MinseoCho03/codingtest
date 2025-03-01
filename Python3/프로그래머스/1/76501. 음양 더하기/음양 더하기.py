f2df55bd25871e64da6d84649b4e7955665713e7
Minseo Cho <105677028+MinseoCho03@users.noreply.github.com>
2025-03-02 00:24:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ Python3/프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,8 @@
 def solution(absolutes, signs):
     result = 0
     for i,j in zip(absolutes, signs):
-        if j == False:
-            result -= i
-        else:
+        if j:
             result += i
+        else:
+            result -= i
     return result
