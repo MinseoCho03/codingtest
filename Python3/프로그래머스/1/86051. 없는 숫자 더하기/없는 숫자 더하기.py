1396b34fa904aa26f54cbdcabbdb7c576aefb147
Minseo Cho <105677028+MinseoCho03@users.noreply.github.com>
2025-03-12 23:27:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 8.99 MB -BaekjoonHub

--- Python3/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ Python3/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,2 @@
 def solution(numbers):
-    answer = []
-    for i in range(10):
-        if i not in numbers:
-            answer.append(i)
-    return sum(answer)
+    return sum(range(10)) - sum(numbers)
