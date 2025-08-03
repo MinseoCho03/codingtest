923ef3c778de3f89393a178733e148b66f2d3fd9
Minseo Cho <105677028+MinseoCho03@users.noreply.github.com>
2025-08-04 01:21:20 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

--- Python3/프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ Python3/프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,30 +1,26 @@
 def solution(a, b, c, d):
-    dice = [a, b, c, d]
-    dice.sort()  
-
-    a, b, c, d = dice 
+    dice = sorted(([a,b,c,d]), reverse = True)
     
-    if a == d:  
-        result = a * 1111
+    if dice[0] == dice[3]:
+        return 1111*dice[0]
+    
+    if dice[0] == dice[2]:
+        return (10 * dice[0] + dice[3])**2
+    
+    if dice[1] == dice[3]:
+        return (10 * dice[1] + dice[0])**2
         
-    elif a == c or b == d:  
-        if a == c:
-            result = (a * 10 + d) ** 2
-        else:
-            result = (b * 10 + a) ** 2
-            
-    elif (a == b) and (c == d):  
-        result = (a + c) * abs(a - c)
-    
-    elif a==b or b==c or c==d:
-        if a==b:
-            result = c*d
-        elif b==c:
-            result = a*d
-        else:
-            result = a*b
-            
-    else:  
-        result = min(dice)
+    if dice[0] == dice[1] and dice[2] == dice[3]:
+        return (dice[0]+dice[2]) * abs(dice[0]-dice[2])
 
-    return result
+    if dice[0] == dice[1]:
+        return dice[2]*dice[3]
+    
+    if dice[1] == dice[2]:
+        return dice[0]*dice[3]
+    
+    if dice[2] == dice[3]:
+        return dice[0]*dice[1]
+    
+    else:
+        return dice[3]
